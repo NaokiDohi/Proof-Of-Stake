a9090f7e200bd957f108ddb4b8b97d44833dad90
NaokiDohi <1210113.naoki.dohi.913@gmail.com>
2022-11-08 20:45:47 +0900

    Create callback methods

--- node/socket/socket_communication.py
+++ node/socket/socket_communication.py
@@ -8,3 +8,20 @@ class SocketCommunication(Node):
 
     def start_socket_communication(self):
         self.start()
+
+    def inbound_node_connected(self, connected_node):
+        print('Inbound Connection')
+        self.send_to_node(
+            connected_node,
+            'Hi! I am the node you connected to.'
+        )
+
+    def outbound_node_connected(self, connected_node):
+        print('Outbound Connection')
+        self.send_to_node(
+            connected_node,
+            'Hi! I am the node who initialized the connection.'
+        )
+
+    def node_message(self, connected_node, message):
+        print(message)
