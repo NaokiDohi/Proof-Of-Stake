b9e0af641f58132c8120607af17a4357947e315c
NaokiDohi <1210113.naoki.dohi.913@gmail.com>
2022-11-09 22:08:10 +0900

    Create some functions for handshake message.

--- node/p2p/peer_discovery_handler.py
+++ node/p2p/peer_discovery_handler.py
@@ -1,5 +1,7 @@
 import time
 import threading
+from utils import Utils
+from node.message import Message
 
 
 class PeerDiscoveryHandler():
@@ -32,4 +34,14 @@ class PeerDiscoveryHandler():
             time.sleep(10)
 
     def handshake(self, connected_node):
-        self.socket_communication.send(connected_node, 'Handshake ...')
+        handshake_message = self.handshake_message()
+        self.socket_communication.send(connected_node, handshake_message)
+
+    def handshake_message(self):
+        own_connector = self.socket_communication.socket_connector
+        own_peers = self.socket_communication.peers
+        data = own_peers
+        message_type = 'DISCOVERY'
+        message = Message(own_connector, message_type, data)
+        encoded_message = Utils().encode(message)
+        return encoded_message
