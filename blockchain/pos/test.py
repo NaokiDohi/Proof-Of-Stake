8e932c974eee66759c8fa3f496945fc59fe5d553
NaokiDohi <1210113.naoki.dohi.913@gmail.com>
2022-11-16 20:35:15 +0900

    Create a Lot class.

--- blockchain/pos/test.py
+++ blockchain/pos/test.py
@@ -2,13 +2,16 @@ import sys  # noqa
 import os  # noqa
 sys.path.append("../../")  # noqa
 from blockchain.pos import ProofOfStake
+from blockchain.pos.lot import Lot
 
 if __name__ == '__main__':
-    pos = ProofOfStake()
-    pos.update('Alice', 100)
-    pos.update('Bob', 10)
-    print(
-        f'Alice:{pos.get("Alice")}\
-        \nBob:{pos.get("Bob")}\
-        \nJack:{pos.get("Jack")}'
-    )
+    # pos = ProofOfStake()
+    # pos.update('Alice', 100)
+    # pos.update('Bob', 10)
+    # print(
+    #     f'Alice:{pos.get("Alice")}\
+    #     \nBob:{pos.get("Bob")}\
+    #     \nJack:{pos.get("Jack")}'
+    # )
+    lot = Lot('bob', 1, 'lastHash')
+    print(lot.lot_hash())
