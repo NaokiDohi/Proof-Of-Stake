ca7849464ea1e792297634c33c78e1f4f5d5a951
NaokiDohi <1210113.naoki.dohi.913@gmail.com>
2022-11-27 21:49:02 +0900

    Update interaction.py for the demonstration.

--- interaction.py
+++ interaction.py
@@ -2,16 +2,27 @@ from blockchain.wallet import Wallet
 from blockchain.utils import Utils
 import requests
 
-if __name__ == '__main__':
-    alice = Wallet()
-    bob = Wallet()
-    exchange = Wallet()
 
-    transaction = exchange.createTransaction(
-        alice.publicKeyString(), 10, 'EXCHANGE'
+def post_transaction(sender, receiver, amount, type):
+    transaction = sender.createTransaction(
+        receiver.publicKeyString(), amount, type
     )
-
     url = 'http://localhost:5001/transaction'
     package = {'transaction': Utils.encode(transaction)}
     request = requests.post(url, json=package)
     print(request.text)
+
+
+if __name__ == '__main__':
+    alice = Wallet()
+    alice.from_key('keys/stakerPrivateKey.pem')
+    bob = Wallet()
+    exchange = Wallet()
+
+    # forger: genesis
+    post_transaction(exchange, alice, 100, 'EXCHANGE')
+    post_transaction(exchange, bob, 100, 'EXCHANGE')
+    post_transaction(alice, alice, 25, 'STAKE')
+
+    # forger: probably alice
+    post_transaction(alice, bob, 1, 'TRANFER')
